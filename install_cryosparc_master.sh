45ea5ae9cc39b9e61a13e7b2547c04062c790696
Steven Vandenbrande <steven.vandenbrande@kuleuven.be>
2023-07-26 12:38:28 +0200

    Link system slurm_foz package into cryoSPARC master environment

--- install_cryosparc_master.sh
+++ install_cryosparc_master.sh
@@ -37,3 +37,11 @@ export master_hostname=$(echo -e "$(hostname -f)" | tr -d '[:space:]')
              --hostname ${master_hostname} \
              --dbpath ${SPARCDIR}/db \
              --port ${port_number}
+
+# There is a bug where jobs cannot be submitted from cryoSPARC, because it
+# sets LD_PRELOAD to its own Python library, which causes problems when
+# submitting a job because the system Python has another version and its
+# packages cannot be found. The temporary workaround is to link required
+# Python packages (slurm_foz) in the cryoSPARC conda environment
+ln -s /usr/lib/python3.6/site-packages/slurm_foz \
+    ${SPARCDIR}/cryosparc_master/deps/anaconda/envs/cryosparc_master_env/lib/python3.8/site-packages/
