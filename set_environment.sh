c5ee594e3f523255dc8c138f59cec0db30ff67b3
Ehsan Moravveji <ehsan.moravveji@kuleuven.be>
2024-07-16 15:00:58 +0200

    adapting the scripts to build on Hortense

--- set_environment.sh
+++ set_environment.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 export LICENSE_ID=
-export SPARCDIR=${VSC_DATA}/cryoSPARC
+export SPARCDIR=${VSC_DATA}/apps/${VSC_INSTITUTE_CLUSTER}/cryoSPARC
 export CREDIT_ACCOUNT=
-export SSD_PATH='${VSC_SCRATCH_NODE}'
+# prepare SSD setup
+cd ${VSC_SCRATCH_NODE}
+test -d ./ssddir || lfs setstripe -E -1 --pool=ssd -c 4 -S 1M ssddir
+export SSD_PATH='${VSC_SCRATCH_NODE}/ssddir'
 
 # Do not edit below
 export PATH=${SPARCDIR}/cryosparc_master/bin:$PATH
