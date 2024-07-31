72b32883df460974dddbb1a338f3a3d04354fa55
Steven Vandenbrande <steven.vandenbrande@kuleuven.be>
2024-07-31 14:57:31 +0200

    Adapt instructions so they also work on Hortense

--- connect_clusters.sh
+++ connect_clusters.sh
@@ -2,8 +2,20 @@
 
 source set_environment.sh
 
-for cluster in genius-cpu genius-p100 genius-p100-debug genius-v100 wice-a100
+if [ $VSC_INSTITUTE_CLUSTER == "dodrio" ]
+then
+    cluster_lanes="hortense-a100"
+elif [ $VSC_INSTITUTE_CLUSTER == "genius" ] || [ $VSC_INSTITUTE_CLUSTER == "wice" ]
+then
+    cluster_lanes="genius-cpu genius-p100 genius-p100-debug genius-v100 wice-a100"
+else
+    echo "Unable to determine cluster lanes for VSC_INSTUTE_CLUSTER=$VSC_INSTITUTE_CLUSTER"
+    exit 1
+fi
+
+for cluster in $cluster_lanes
 do
+    echo "Adding cluster lane $cluster"
     cp cluster_info/${cluster}.json cluster_info.json
     cp cluster_info/${cluster}.sh cluster_script.sh
     sed -i "s|__credit_account__|${CREDIT_ACCOUNT}|" cluster_script.sh
