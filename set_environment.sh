789e6256e3bf927eee13bdfcef806a2eab5b90ca
Ehsan Moravveji <ehsan.moravveji@kuleuven.be>
2024-08-14 12:02:31 +0200

    removing personal license id

--- set_environment.sh
+++ set_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LICENSE_ID='72461aba-4438-11ef-a483-d77ff7d7538e'
+export LICENSE_ID=
 export SPARCDIR=${VSC_DATA}/cryoSPARC
 export CREDIT_ACCOUNT='l_sysadmin'
 export SSD_PATH=${VSC_SCRATCH_NODE}
